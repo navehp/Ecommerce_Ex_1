2d0dfd89f38b3a801716e12a606a4760fea3fb2f
Liron Tyomkin <liron.tyomkin@gmail.com>
2021-05-10 16:50:16 +0300

    added hw2 base files

---
+++ HW/HW2/ex2.py
@@ -0,0 +1,101 @@
+import abc
+from typing import Tuple
+import pandas as pd
+import numpy as np
+
+
+class Recommender(abc.ABC):
+    def __init__(self, ratings: pd.DataFrame):
+        self.initialize_predictor(ratings)
+
+    @abc.abstractmethod
+    def initialize_predictor(self, ratings: pd.DataFrame):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def predict(self, user: int, item: int, timestamp: int) -> float:
+        """
+        :param user: User identifier
+        :param item: Item identifier
+        :param timestamp: Rating timestamp
+        :return: Predicted rating of the user for the item
+        """
+        raise NotImplementedError()
+
+    def rmse(self, true_ratings) -> float:
+        """
+        :param true_ratings: DataFrame of the real ratings
+        :return: RMSE score
+        """
+        pass
+
+
+class BaselineRecommender(Recommender):
+    def initialize_predictor(self, ratings: pd.DataFrame):
+        pass
+
+    def predict(self, user: int, item: int, timestamp: int) -> float:
+        """
+        :param user: User identifier
+        :param item: Item identifier
+        :param timestamp: Rating timestamp
+        :return: Predicted rating of the user for the item
+        """
+        pass
+
+
+class NeighborhoodRecommender(Recommender):
+    def initialize_predictor(self, ratings: pd.DataFrame):
+        pass
+
+    def predict(self, user: int, item: int, timestamp: int) -> float:
+        """
+        :param user: User identifier
+        :param item: Item identifier
+        :param timestamp: Rating timestamp
+        :return: Predicted rating of the user for the item
+        """
+        pass
+
+    def user_similarity(self, user1: int, user2: int) -> float:
+        """
+        :param user1: User identifier
+        :param user2: User identifier
+        :return: The correlation of the two users (between -1 and 1)
+        """
+        pass
+
+
+class LSRecommender(Recommender):
+    def initialize_predictor(self, ratings: pd.DataFrame):
+        pass
+
+    def predict(self, user: int, item: int, timestamp: int) -> float:
+        """
+        :param user: User identifier
+        :param item: Item identifier
+        :param timestamp: Rating timestamp
+        :return: Predicted rating of the user for the item
+        """
+        pass
+
+    def solve_ls(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Creates and solves the least squares regression
+        :return: Tuple of X, b, y such that b is the solution to min ||Xb-y||
+        """
+        pass
+
+
+class CompetitionRecommender(Recommender):
+    def initialize_predictor(self, ratings: pd.DataFrame):
+        pass
+
+    def predict(self, user: int, item: int, timestamp: int) -> float:
+        """
+        :param user: User identifier
+        :param item: Item identifier
+        :param timestamp: Rating timestamp
+        :return: Predicted rating of the user for the item
+        """
+        pass
---
+++ HW/HW2/main.py
@@ -0,0 +1,69 @@
+import ex2
+import pandas as pd
+import numpy as np
+import time
+
+
+def transform(ratings, min_items=5, min_users=7):
+    """
+    Transforms ratings DataFrame
+    :param ratings: Ratings DataFrame
+    :param min_items: Minimum items per user
+    :param min_users: Minimum users per item
+    :return: Transformed DataFrame
+    """
+    ratings = ratings.groupby('user').filter(lambda items: len(items) >= min_items)
+    ratings = ratings.groupby('item').filter(lambda users: len(users) >= min_users)
+    unique_users = ratings['user'].unique()
+    unique_items = ratings['item'].unique()
+    user_mapping = {u: k for k, u in enumerate(unique_users)}
+    item_mapping = {i: k for k, i in enumerate(unique_items)}
+    ratings = ratings.replace({'user': user_mapping, 'item': item_mapping})
+    return ratings
+
+
+def train_test_split(ratings, train_ratio=0.8):
+    """
+    Splits ratings per user
+    :param ratings: Ratings DataFrame
+    :param train_ratio: Percentage of ratings in the train set
+    :return: A tuple of train and test DataFrames
+    """
+    train, test = [], []
+    for user in ratings.groupby('user'):
+        rows = user[1].values.tolist()
+        split = int(train_ratio * len(rows))
+        indices = np.random.permutation(len(rows))
+        for i, row in enumerate(rows):
+            if i in indices[:split]:
+                train.append(row)
+            else:
+                test.append(row)
+    train = pd.DataFrame(train, columns=ratings.columns, index=None)
+    test = pd.DataFrame(test, columns=ratings.columns, index=None)
+    return train, test
+
+
+def main():
+    ratings = transform(pd.read_csv('ratings.csv'))
+    train, test = train_test_split(ratings)
+    start = time.time()
+
+    baseline_recommender = ex2.BaselineRecommender(train)
+    print(baseline_recommender.rmse(test))
+    neighborhood_recommender = ex2.NeighborhoodRecommender(train)
+    print(neighborhood_recommender.rmse(test))
+    ls_recommender = ex2.LSRecommender(train)
+    ls_recommender.solve_ls()
+    print(ls_recommender.rmse(test))
+
+    # ratings_comp = pd.read_csv('ratings_comp.csv')
+    # comp_recommender = ex2.CompetitionRecommender(ratings_comp)
+    # print(comp_recommender.rmse(test_comp))
+
+    print(f'Took {time.time() - start:.2f}s')
+
+
+if __name__ == '__main__':
+    np.random.seed(0)
+    main()
