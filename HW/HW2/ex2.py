cc822088f44030749700dd3b9c67ecf1f87dfc5a
navehp <63583024+navehp@users.noreply.github.com>
2021-05-10 13:30:15 +0300

    Finished first path HW2, some mistakes

---
+++ HW/HW2/ex2.py
@@ -0,0 +1,131 @@
+import abc
+from typing import Tuple
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+class Recommender(abc.ABC):
+    def __init__(self, ratings: pd.DataFrame):
+        self.initialize_predictor(ratings)
+
+    @abc.abstractmethod
+    def initialize_predictor(self, ratings: pd.DataFrame):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def predict(self, user: int, item: int, timestamp: int) -> float:
+        """
+        :param user: User identifier
+        :param item: Item identifier
+        :param timestamp: Rating timestamp
+        :return: Predicted rating of the user for the item
+        """
+        raise NotImplementedError()
+
+    def rmse(self, true_ratings) -> float:
+        """
+        :param true_ratings: DataFrame of the real ratings
+        :return: RMSE score
+        """
+        r = true_ratings['rating']
+        p = true_ratings.apply(lambda x: self.predict(int(x['user']), int(x['item']), x['timestamp']), axis=1)
+        return np.sqrt(((r - p) ** 2).sum() / len(r))
+
+
+class BaselineRecommender(Recommender):
+    def initialize_predictor(self, ratings: pd.DataFrame):
+        self.R_hat = ratings.rating.mean()
+        self.b_u = ratings.groupby('user').rating.mean() - self.R_hat
+        self.b_i = ratings.groupby('item').rating.mean() - self.R_hat
+
+    def predict(self, user: int, item: int, timestamp: int) -> float:
+        """
+        :param user: User identifier
+        :param item: Item identifier
+        :param timestamp: Rating timestamp
+        :return: Predicted rating of the user for the item
+        """
+        p = self.R_hat + self.b_u[user] + self.b_i[item]
+        if p < 0.5 or 5 < p:
+            print(1, p)
+        return self.R_hat + self.b_u[user] + self.b_i[item]
+
+
+class NeighborhoodRecommender(Recommender):
+    def initialize_predictor(self, ratings: pd.DataFrame):
+        self.R_hat = ratings.rating.mean()
+        self.centered_ratings = ratings.copy()
+        self.centered_ratings.rating = self.centered_ratings.rating - self.R_hat
+        self.b_u = ratings.groupby('user').rating.mean() - self.R_hat
+        self.b_i = ratings.groupby('item').rating.mean() - self.R_hat
+
+        ratings_matrix = pd.pivot_table(self.centered_ratings, values='rating', index='user', columns=['item']).fillna(0)
+        self.user_corrs = cosine_similarity(ratings_matrix, ratings_matrix)
+
+    def predict(self, user: int, item: int, timestamp: int) -> float:
+        """
+        :param user: User identifier
+        :param item: Item identifier
+        :param timestamp: Rating timestamp
+        :return: Predicted rating of the user for the item
+        """
+        user_correlations = self.user_corrs[user]
+        top_correlations_indices = user_correlations.argsort()[-4:-1]
+        neighbors_arg = 0
+        correlations_sum = 0
+        for neighbor in top_correlations_indices:
+            correlation = user_correlations[neighbor]
+            neighbor_ratings = self.centered_ratings[self.centered_ratings.user == neighbor]
+            rating = neighbor_ratings[neighbor_ratings.item == item].rating
+            if len(rating) > 0:
+                neighbors_arg += correlation * float(rating)
+            correlations_sum += np.abs(correlation)
+        p = self.R_hat + self.b_u[user] + self.b_i[item] + neighbors_arg / correlations_sum
+        if p < 0.5 or 5 < p:
+            print(p)
+        return self.R_hat + self.b_u[user] + self.b_i[item] + neighbors_arg / correlations_sum
+
+    def user_similarity(self, user1: int, user2: int) -> float:
+        """
+        :param user1: User identifier
+        :param user2: User identifier
+        :return: The correlation of the two users (between -1 and 1)
+        """
+        return self.user_corrs[user1][user2]
+
+
+class LSRecommender(Recommender):
+    def initialize_predictor(self, ratings: pd.DataFrame):
+        pass
+
+    def predict(self, user: int, item: int, timestamp: int) -> float:
+        """
+        :param user: User identifier
+        :param item: Item identifier
+        :param timestamp: Rating timestamp
+        :return: Predicted rating of the user for the item
+        """
+        pass
+
+    def solve_ls(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Creates and solves the least squares regression
+        :return: Tuple of X, b, y such that b is the solution to min ||Xb-y||
+        """
+        pass
+
+
+class CompetitionRecommender(Recommender):
+    def initialize_predictor(self, ratings: pd.DataFrame):
+        pass
+
+    def predict(self, user: int, item: int, timestamp: int) -> float:
+        """
+        :param user: User identifier
+        :param item: Item identifier
+        :param timestamp: Rating timestamp
+        :return: Predicted rating of the user for the item
+        """
+        pass
